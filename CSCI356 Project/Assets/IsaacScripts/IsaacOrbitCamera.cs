4a5e6e59c19c54882a6241afae68de1c4a5bb455
JimmyNgJE <147128573+JimmyNgJE@users.noreply.github.com>
2024-08-22 15:40:20 +0800

    Orbit Camera changed

--- CSCI356 Project/Assets/IsaacScripts/IsaacOrbitCamera.cs
+++ CSCI356 Project/Assets/IsaacScripts/IsaacOrbitCamera.cs
@@ -1,44 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class IsaacOrbitCamera : MonoBehaviour
 {
-    // the orbit target (e.g., the player GameObject)
     [SerializeField] Transform target;
-
-    // rotation sensitivity
     public float rotSpeed = 1.5f;
+    public float distance = 5.0f; // Distance from the player
+    public float height = 2.0f; // Height above the player
 
-    private float rotY;     // horizontal rotation
+    private float rotY;
     private float rotX;
-    private Vector3 offset; // offset from the target
 
-    // Start is called before the first frame update
     void Start()
     {
-        // get transform component's yaw
         rotY = transform.eulerAngles.y;
         rotX = transform.eulerAngles.x;
-
-        // calculate camera's offset from the target
-        offset = target.position - transform.position;
     }
 
-    // LateUpdate is called once per frame after Update
     void LateUpdate()
     {
-        // yaw based on horizontal mouse movement
-        rotY += Input.GetAxis("Mouse X") * rotSpeed * 3;
-        rotX += Input.GetAxis("Mouse Y") * rotSpeed * 3;
+        rotY += Input.GetAxis("Mouse X") * rotSpeed;
+        rotX -= Input.GetAxis("Mouse Y") * rotSpeed;
+        rotX = Mathf.Clamp(rotX, -20f, 60f); // Limit vertical angle
 
-        // create quaternion based on rotation angle
         Quaternion rotation = Quaternion.Euler(rotX, rotY, 0);
 
-        // set the camera's position based on the rotated offset
-        transform.position = target.position - (rotation * offset);
+        // Calculate the desired camera position
+        Vector3 position = target.position - (rotation * Vector3.forward * distance);
+        position.y += height;
 
-        // rotate camera to look at the target
+        transform.position = position;
         transform.LookAt(target);
+
+        // Make the player rotate with the camera
+        target.rotation = Quaternion.Euler(0, rotY, 0);
     }
 }
